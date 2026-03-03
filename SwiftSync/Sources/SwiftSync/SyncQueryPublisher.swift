fff8358e5886947f0988bbfb1743ab7be5142f03
Elvis Nunez <elvisnunez@me.com>
2026-03-03 10:03:58 +0100

    Remove unused SyncQueryPublisher inits

--- SwiftSync/Sources/SwiftSync/SyncQueryPublisher.swift
+++ SwiftSync/Sources/SwiftSync/SyncQueryPublisher.swift
@@ -14,24 +14,19 @@ public final class SyncQueryPublisher<Model: PersistentModel>: ObservableObject,
     // MARK: - Private state
 
     private let syncContainer: SyncContainer
-    private let predicate: Predicate<Model>?
     private let sortBy: [SortDescriptor<Model>]
-    private let postFetchFilter: ((Model) -> Bool)?
     private let observedModelTypeNames: Set<String>
     private var notificationToken: NSObjectProtocol?
 
-    // MARK: - Designated init
+    // MARK: - Public init
 
-    private init(
-        syncContainer: SyncContainer,
-        predicate: Predicate<Model>?,
-        sortBy: [SortDescriptor<Model>],
-        postFetchFilter: ((Model) -> Bool)?
+    public init(
+        _ _: Model.Type,
+        in syncContainer: SyncContainer,
+        sortBy: [SortDescriptor<Model>] = []
     ) {
         self.syncContainer = syncContainer
-        self.predicate = predicate
         self.sortBy = sortBy
-        self.postFetchFilter = postFetchFilter
         self.observedModelTypeNames = Self.defaultObservedModelTypeNames()
         notificationToken = NotificationCenter.default.addObserver(
             forName: SyncContainer.didSaveChangesNotification,
@@ -42,62 +37,6 @@ public final class SyncQueryPublisher<Model: PersistentModel>: ObservableObject,
             self.reload()
         }
         reload()
-    }
-
-    // MARK: - Public inits
-
-    public convenience init(
-        _ _: Model.Type,
-        in syncContainer: SyncContainer,
-        sortBy: [SortDescriptor<Model>] = []
-    ) {
-        self.init(syncContainer: syncContainer, predicate: nil, sortBy: sortBy, postFetchFilter: nil)
-    }
-
-    public convenience init(
-        _ _: Model.Type,
-        predicate: Predicate<Model>,
-        in syncContainer: SyncContainer,
-        sortBy: [SortDescriptor<Model>] = []
-    ) {
-        self.init(syncContainer: syncContainer, predicate: predicate, sortBy: sortBy, postFetchFilter: nil)
-    }
-
-    public convenience init<Related: SyncModelable>(
-        _ _: Model.Type,
-        relatedTo _: Related.Type,
-        relatedID: Related.SyncID,
-        through relationship: ReferenceWritableKeyPath<Model, Related?>,
-        in syncContainer: SyncContainer,
-        sortBy: [SortDescriptor<Model>] = []
-    ) {
-        self.init(
-            syncContainer: syncContainer,
-            predicate: nil,
-            sortBy: sortBy,
-            postFetchFilter: { row in
-                guard let related = row[keyPath: relationship] else { return false }
-                return related[keyPath: Related.syncIdentity] == relatedID
-            }
-        )
-    }
-
-    public convenience init<Related: SyncModelable>(
-        _ _: Model.Type,
-        relatedTo _: Related.Type,
-        relatedID: Related.SyncID,
-        through relationship: ReferenceWritableKeyPath<Model, [Related]>,
-        in syncContainer: SyncContainer,
-        sortBy: [SortDescriptor<Model>] = []
-    ) {
-        self.init(
-            syncContainer: syncContainer,
-            predicate: nil,
-            sortBy: sortBy,
-            postFetchFilter: { row in
-                row[keyPath: relationship].contains { $0[keyPath: Related.syncIdentity] == relatedID }
-            }
-        )
     }
 
     // MARK: - Teardown
@@ -121,10 +60,8 @@ public final class SyncQueryPublisher<Model: PersistentModel>: ObservableObject,
     }
 
     private func reload() {
-        let descriptor = predicate.map { FetchDescriptor(predicate: $0, sortBy: sortBy) }
-            ?? FetchDescriptor(sortBy: sortBy)
-        let fetched = (try? syncContainer.mainContext.fetch(descriptor)) ?? []
-        rows = postFetchFilter.map { fetched.filter($0) } ?? fetched
+        let descriptor = FetchDescriptor<Model>(sortBy: sortBy)
+        rows = (try? syncContainer.mainContext.fetch(descriptor)) ?? []
     }
 
     private static func defaultObservedModelTypeNames() -> Set<String> {
