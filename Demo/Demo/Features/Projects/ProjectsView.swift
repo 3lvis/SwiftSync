f1af0ef8c7725f299479242be56200c8887b53fb
Elvis Nunez <elvisnunez@me.com>
2026-03-08 19:59:37 +0100

    Use single file for UIViewControllerRepresentable

--- Demo/Demo/Features/Projects/ProjectsView.swift
+++ Demo/Demo/Features/Projects/ProjectsView.swift
@@ -2,6 +2,18 @@ import DemoCore
 import SwiftSync
 import SwiftUI
 
+struct ProjectsRepresentable: UIViewControllerRepresentable {
+    let syncContainer: SyncContainer
+    let syncEngine: DemoSyncEngine
+    let onSelect: (String) -> Void
+
+    func makeUIViewController(context: Context) -> ProjectsViewController {
+        ProjectsViewController(syncContainer: syncContainer, syncEngine: syncEngine, onSelect: onSelect)
+    }
+
+    func updateUIViewController(_ uiViewController: ProjectsViewController, context: Context) {}
+}
+
 struct ProjectsView: View {
     let syncContainer: SyncContainer
     @ObservedObject var syncEngine: DemoSyncEngine
